5c75d1a99b9bdca0287560fc639c31bafe5c4711
Oxke <oseaetobia@gmail.com>
2023-12-03 20:40:43 +0100

    AutoCommit

--- ytfp.sh
+++ ytfp.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script is used to execute the fp.py script
 
-sh -c "/usr/bin/python ~/.local/share/ulauncher/extensions/com.github.oxke.ulauncher-ytfp/fp.py >> ~/.local/share/ulauncher/extensions/com.github.oxke.ulauncher-ytlazy/fp.log"
