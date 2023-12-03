b9d8db60d028a96175ebd6acfeebb1267a2c74da
Oxke <oseaetobia@gmail.com>
2023-12-03 20:01:54 +0100

    rename

--- install.sh
+++
@@ -1,6 +0,0 @@
-here=~/.local/share/ulauncher/extensions/com.github.oxke.ulauncher-ytwl
-pip install $here/requirements.txt
-sudo cp $here/ytfp.* /etc/systemd/system/
-systemctl enable ytfp.timer
-touch $here/subscriptions2
-touch $here/watchlist2
